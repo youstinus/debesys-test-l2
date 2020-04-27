a344f82ab60b84be527702b5974d3e5334e1aaf2
youstinus <youstinus@gmail.com>
2020-04-27 11:29:19 +0300

    Add project files.

---
+++ Lab2Test1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab1Test1.MvcApplication" Language="C#" %>
---
+++ Lab2Test1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab1Test1.Startup))]
+namespace Lab1Test1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
